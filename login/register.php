48fde62f4c7ec727fbca5d8930626b38d3b86ffc
Jeyaruban jenushan <jjenushan550@gmail.com>
2024-10-16 14:09:37 +0530

    newchanges

--- login/register.php
+++ login/register.php
@@ -1,5 +1,5 @@
 <?php
-include "./config.php";
+include "../config.php";
 
 //INSERT THE DATA
 if(isset($_POST['submit'])){
@@ -33,7 +33,7 @@ header('location:login.php') ;
     <title>Register</title>
     <!--font awesome cdn inks-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 
