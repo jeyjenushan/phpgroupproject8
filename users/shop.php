f793779cf0e7f38afc28e47822ef6ee4de70d05f
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2024-10-23 15:38:34 +0530

    shop pag,contact us page css heading edit

--- users/shop.php
+++ users/shop.php
@@ -33,12 +33,13 @@ if (isset($_POST['add_to_cart'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/shop_style.css">
     <title>Shop</title>
 </head>
 
 <body>
     <?php include './user_header.php' ?>
-    <div class="heading">
+    <div class="shop_heading">
         <h3>our shop</h3>
         <p> <a href="home.php">home</a> / shop </p>
     </div>
